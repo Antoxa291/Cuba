c5bc7af24ee86b5265bb93a42802340ab489094d
toxa <antoxa2918@gmail.com>
2020-08-05 21:07:30 +0300

    add slick slider

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
-$(function(){
+$(function () {
 
+    $('.photo__slider').slick({
+        // arrows: false,
+        prevArrow: '<button type="button" class="slick-prev"><img src="images/arrow-left.svg" alt=""></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="images/arrow-right.svg" alt=""></button>'
+    });
 });
