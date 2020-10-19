202da8f40366e6ae6e9f1bee7648529b8dc60384
toxa <antoxa2918@gmail.com>
2020-10-19 14:19:48 +0300

    add: codelac

--- js/main.js
+++ js/main.js
@@ -5,4 +5,9 @@ $(function () {
         prevArrow: '<button type="button" class="slick-prev"><img src="images/arrow-left.svg" alt=""></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="images/arrow-right.svg" alt=""></button>'
     });
+
+    $('.menu__btn').on('click', function () {
+        $('.menu__list').slideToggle();
+    });
+
 });
